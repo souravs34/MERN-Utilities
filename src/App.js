4fd549f2f92a77a07b937688fc7e2206189c1ea0
Sourav34 <borateourav@gmail.com>
2021-09-14 18:43:27 +0530

    Youtube Tutorial

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import UseStateCounter from "./components/1-useState/5-useState-counter";
+function App() {
+  return (
+    <div className="container">
+      <UseStateCounter />
+    </div>
+  );
+}
+
+export default App;
